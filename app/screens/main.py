623b5e27d5e86d9d12585d8a60f7155ddb8ca922
Brent <brentkammen@yahoo.com>
2017-10-03 14:45:29 -0600

    Updated python code
    Updated code on raspberry pi and ran it as normal.  added image grabs, need to work on continually updates for images other wise system only get image at start up and not continually.
    
    Still work in progress

--- app/screens/main.py
+++ app/screens/main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-import pygame
+import pygame, urllib, cStringIO
 from pygame.mixer import Sound
+from PIL import Image
 
 from ui import colours
 from ui.widgets.background import LcarsBackgroundImage, LcarsImage
@@ -13,19 +14,33 @@ class ScreenMain(LcarsScreen):
     def setup(self, all_sprites):
         all_sprites.add(LcarsBackgroundImage("assets/lcars_screen_1b.png"),
                         layer=0)
-        
+	# Image reference
+	WINTERPARKSNOW = cStringIO.StringIO(urllib.urlopen("http://cams.winterparkresort.com/winter-park-basecam.jpg").read())
+	WINTERPARKSNOW2 = Image.open(WINTERPARKSNOW)
+	WINTERPARK2 = WINTERPARKSNOW2.resize((380,232), Image.ANTIALIAS)
+	WINTERPARK2.save("WINTERPARKSNOW.jpg")
+	KEYSTONESNOW = cStringIO.StringIO(urllib.urlopen("http://common.snow.com/mtncams/riverrun.jpg").read())
+        KEYSTONESNOW2 = Image.open(KEYSTONESNOW)
+        KEYSTONE2 = KEYSTONESNOW2.resize((380,232), Image.ANTIALIAS)
+        KEYSTONE2.save("KEYSTONESNOW.jpg")      
+	BRECKENRIDGESNOW = cStringIO.StringIO(urllib.urlopen("http://common.snow.com/Mtncams/brecksnowcam.jpg").read())
+        BRECKENRIDGESNOW2 = Image.open(BRECKENRIDGESNOW)
+        BRECKENRIDGE2 = BRECKENRIDGESNOW2.resize((380,232), Image.ANTIALIAS)
+        BRECKENRIDGE2.save("BRECKENRIDGESNOW.jpg")
+
+
         # panel text
-        all_sprites.add(LcarsText(colours.BLACK, (15, 44), "SNOW REPORT"),
+        all_sprites.add(LcarsText(colours.BLACK, (15, 44), "COLORADO"),
                         layer=1)
-        all_sprites.add(LcarsText(colours.ORANGE, (0, 135), "COLORADO", 2),
+        all_sprites.add(LcarsText(colours.ORANGE, (0, 135), "SNOW REPORT", 2),
                         layer=1)
-        all_sprites.add(LcarsBlockMedium(colours.RED_BROWN, (145, 16), "LIGHTS"),
+        all_sprites.add(LcarsBlockMedium(colours.RED_BROWN, (145, 16), "WEATHER"),
                         layer=1)
         all_sprites.add(LcarsBlockSmall(colours.ORANGE, (211, 16), "CAMERAS"),
                         layer=1)
         all_sprites.add(LcarsBlockLarge(colours.BEIGE, (249, 16), "ENERGY"),
                         layer=1)
-        all_sprites.add(LcarsText(colours.BLACK, (444, 612), "192 168 0 3"),
+        all_sprites.add(LcarsText(colours.BLACK, (444, 612), "172 18 0 99"),
                         layer=1)
 
         # info text
@@ -35,9 +50,9 @@ class ScreenMain(LcarsScreen):
                         layer=3)
         all_sprites.add(LcarsText(colours.BLUE, (286, 174), "ARAPAHOE BASEN: MID OCTOBER", 1.5),
                         layer=3)
-        all_sprites.add(LcarsText(colours.BLUE, (330, 174), "LOVELAND: MID OCTOBER", 1.5),
+        all_sprites.add(LcarsText(colours.BLUE, (330, 174), "KEYSTONE: NOVEMBER 10, 2017", 1.5),
                         layer=3)
-        all_sprites.add(LcarsText(colours.BLUE, (370, 174), "VAIL:  NOVEMBER 17, 2017", 1.5),
+        all_sprites.add(LcarsText(colours.BLUE, (370, 174), "BRECKENRIDGE:  NOVEMBER 10, 2017", 1.5),
                         layer=3)
         self.info_text = all_sprites.get_sprites_from_layer(3)
 
@@ -49,27 +64,35 @@ class ScreenMain(LcarsScreen):
         # buttons        
         all_sprites.add(LcarsButton(colours.RED_BROWN, (6, 662), "LOGOUT", self.logoutHandler),
                         layer=4)
-        all_sprites.add(LcarsButton(colours.RED, (107, 127), "WINTER PARK", self.sensorsHandler),
+        #all_sprites.add(LcarsButton(colours.BEIGE, (107, 127), "DASHBOARD", self.sensorsHandler),
+        #                layer=4)
+        all_sprites.add(LcarsButton(colours.PURPLE, (107, 262), "BRECKENRIDGE", self.sensorsHandler),
                         layer=4)
-        all_sprites.add(LcarsButton(colours.PURPLE, (107, 262), "GAUGES", self.gaugesHandler),
+        all_sprites.add(LcarsButton(colours.BEIGE, (107, 127), "KEYSTONE", self.gaugesHandler),
                         layer=4)
-        all_sprites.add(LcarsButton(colours.PEACH, (107, 398), "WEATHER", self.weatherHandler),
+        #all_sprites.add(LcarsButton(colours.PEACH, (107, 398), "WINTER PARK", self.weatherHandler),
+        #                layer=4)
+        all_sprites.add(LcarsButton(colours.PEACH, (107, 398), "WINTER PARK", self.weatherHandler),
                         layer=4)
 
         # gadgets        
         all_sprites.add(LcarsGifImage("assets/gadgets/fwscan.gif", (277, 556), 100), layer=1)
         
-        self.sensor_gadget = LcarsGifImage("https://www.winterparkresort.com/the-mountain/mountain-cams/snow-stake", (235, 150), 100) 
+        self.sensor_gadget = LcarsImage ("BRECKENRIDGESNOW.jpg", (200, 130)) 
         self.sensor_gadget.visible = False
         all_sprites.add(self.sensor_gadget, layer=2)
 
-        self.dashboard = LcarsImage("assets/gadgets/dashboard.png", (187, 232))
+        self.dashboard = LcarsImage("KEYSTONESNOW.jpg", (200, 130))
         self.dashboard.visible = False
         all_sprites.add(self.dashboard, layer=2) 
 
-        self.weather = LcarsImage("assets/weather.jpg", (188, 122))
-        self.weather.visible = False
+        self.weather = LcarsImage("WINTERPARKSNOW.jpg", (200, 130))
+	self.weather.visible = False
         all_sprites.add(self.weather, layer=2) 
+
+        self.gauges = LcarsImage("KEYSTONESNOW.jpg", (200, 130))
+        self.gauges.visible = False
+        all_sprites.add(self.weather, layer=2)
 
         #all_sprites.add(LcarsMoveToMouse(colours.WHITE), layer=1)
         self.beep1 = Sound("assets/audio/panel/201.wav")
